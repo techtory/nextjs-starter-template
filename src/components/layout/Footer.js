43414a3342d7fb06b507d068202f546647e090b3
Samroon Michael <samroonmichael@gmail.com>
2021-05-10 17:04:36 +0800

    Add style-components lib && global styles

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,24 +1,34 @@
-import Image from 'next/image'
+// Add scripts here
 
 /** Style-sheet **/
-import styles from '../../../styles/Home.module.css'
+import styled from 'styled-components'
+
+/*
+	These are default styles you can over-ride them according to need of your project.
+*/
+
+// Footer
+const FooterWapper = styled.footer`
+	width: 100%;
+	height: 100px;
+	border-top: 1px solid #eaeaea;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
 
 const Footer = () => {
 	return (
 		<>
-			<footer className={styles.footer}>
+			<FooterWapper>
 				<a
 					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Powered by
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
+					Copyright Techtroy 2021
 				</a>
-				<h3>Copyright 2021</h3>
-			</footer>
+			</FooterWapper>
 		</>
 	)
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,28 @@
+// Add scripts here
+
+/** Style-sheet **/
+import styled from 'styled-components'
+
+/*
+	These are default styles you can over-ride them according to need of your project.
+*/
+
+// Header
+const HeaderWapper = styled.header`
+	width: 100%;
+	height: 100px;
+	border-top: 1px solid #eaeaea;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
 const Header = () => (
 	<>
-		<header>
-			<h2>Logo</h2>
+		<HeaderWapper>
+			<h2 style={{ marginRight: '1rem' }}>Logo</h2>
 			<h1>Menu</h1>
-		</header>
+		</HeaderWapper>
 	</>
 )
 
